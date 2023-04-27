37cc407728637aad5d16474521b109badf43ebb4
zhanghang <672683505@qq.com>
2023-04-27 18:31:48 +0800

    footer

--- miniprogram/components/footer/index.js
+++ miniprogram/components/footer/index.js
@@ -2,19 +2,40 @@
 
 Component({
   // footer 
-  
+    attached:function(){
+      let pages = getCurrentPages()
+      let currentPage = pages[pages.length - 1]
+      let url = currentPage.route;
+      console.log(url)
+      if(url){
+        switch(url){
+          case 'pages/student/joinClass':
+            console.log(1112)
+            this.setData({active:'classPaper'})
+            break;
+          case 'pages/student/test':
+            console.log(1113)
+            this.setData({active:'test'})
+            break;
+        }
+      }
+
+
+    }
+  ,
   data:{
-    active: 'home',
+    active: 'friends',
   },
   methods: {
     onChange(event) {
       this.setData({ active: event.detail });
+      console.log(event.detail)
       if(event.detail == 'classPaper'){
-        wx.navigateTo({
+        wx.redirectTo({
           url: '/pages/student/joinClass',
         })
       }else if(event.detail == 'test'){
-        wx.navigateTo({
+        wx.redirectTo({
           url: '/pages/student/test',
         })
       }
